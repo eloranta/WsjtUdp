0ff3e44e83a4695a45d0672f462d050d2682423a
hannu <hannu.eloranta@gmail.com>
2024-01-10 09:34:34 +0200

    add splitting message

--- Alert/mainwindow.cpp
+++ Alert/mainwindow.cpp
@@ -19,9 +19,25 @@ MainWindow::~MainWindow()
 void MainWindow::MessageReceived(const QString& message)
 {
     qDebug() << message;
-    QStandardItem *item1 = new QStandardItem(message);
-    model.setItem(y++, 0, item1);
-
+    if (message.isEmpty())
+        return;
+    const QStringList list = message.split(" ");
+    if (list.length() > 0)
+    {
+        QStandardItem *item = new QStandardItem(list[0]);
+        model.setItem(y, 0, item);
+    }
+    if (list.length() > 1)
+    {
+        QStandardItem *item = new QStandardItem(list[1]);
+        model.setItem(y, 1, item);
+    }
+    if (list.length() > 2)
+    {
+        QStandardItem *item = new QStandardItem(list[2]);
+        model.setItem(y, 2, item);
+    }
+    y++;
 }
 
 
