bdff64b520e6ee06187da5851ca1b4d3549065b9
hannu <hannu.eloranta@gmail.com>
2024-01-15 15:50:43 +0200

    copied alert over nac code

--- Nac/mainwindow.cpp
+++ Nac/mainwindow.cpp
@@ -6,10 +6,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableView->setModel(&model);
+    y = 0;
+    connect(&udp, SIGNAL(MessageReceived(const QString&)), this, SLOT(MessageReceived(const QString&)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::MessageReceived(const QString& message)
+{
+    qDebug() << message;
+    if (message.isEmpty())
+        return;
+
+    const QStringList list = message.split(" ");
+    if (list.length() == 1)
+        return;
+
+    QStandardItem *item = new QStandardItem(list[1]);
+    if (list.length() >= 2 && list[1].length() == 2)
+        item = new QStandardItem(list[2]);
+    model.setItem(y, 0, item);
+
+    item = new QStandardItem(message);
+    model.setItem(y, 1, item);
+
+    y++;
+    ui->tableView->scrollToBottom();
+}
+
 
--- Nac/mainwindow.h
+++ Nac/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStandardItemModel>
+#include "../Common/udp.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,8 +16,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public slots:
+    void MessageReceived(const QString& message);
 
 private:
     Ui::MainWindow *ui;
+    Udp udp;
+    QStandardItemModel model;
+    int y;
 };
 #endif // MAINWINDOW_H
